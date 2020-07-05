f92ca09ae8853eedf90648a6643a53295af2444c
alanxgorlan <alanxgorlan@163.com>
2020-07-05 23:06:20 +0800

    DataProvider参数化学习二，使用Method给多个方法提供参数

--- Chapter5/src/main/java/com/javatest/firsttry/parameters/DataProviderTest.java
+++ Chapter5/src/main/java/com/javatest/firsttry/parameters/DataProviderTest.java
@@ -3,6 +3,8 @@ package com.javatest.firsttry.parameters;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.lang.reflect.Method;
+
 public class DataProviderTest {
     @Test(dataProvider = "data")
     public void testDataProvider(String name,int age){
@@ -16,5 +18,34 @@ public class DataProviderTest {
                 {"Tom",20}
         };
         return o;
+    }
+
+    //使用Method来区分方法，从而为多个方法提供参数
+    @Test(dataProvider = "methodtest")
+    public void test1(String name,int age){
+        System.out.println("test1 name=" + name +" ;age=" + age );
+    }
+
+    @Test(dataProvider = "methodtest")
+    public void test2(String name,int age){
+        System.out.println("test2 name=" + name +" ;age=" + age );
+    }
+
+    @DataProvider(name = "methodtest")
+    public Object[][] methodTest(Method method){
+        Object[][] res =null;
+
+        if(method.getName().equals("test1")){
+            res = new Object[][]{
+                    {"FFFFFF",30}
+            };
+        }else if (method.getName().equals("test2")){
+            res = new Object[][]{
+                    {"OOOOOO",30}
+            };
+        }
+
+        return res;
+
     }
 }
